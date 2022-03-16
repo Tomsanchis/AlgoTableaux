d626f5123cb69a70f269060ccddaa3e1a7a1597c
Tomsanchis <92373414+Tomsanchis@users.noreply.github.com>
2022-03-16 11:12:44 +0100

    import & describe for moy, min

--- test/test.js
+++ test/test.js
@@ -1,8 +1,15 @@
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+import assert from 'assert';
+import { min } from '../js/app.js';
+import { moy } from '../js/app.js';
+
+describe('min', () => {
+    it('should return min of tb', () => {
+      assert.equal(min([5, 6, 2, 7, 1]), 1);
     });
-  });
-});
+})
+
+describe('moy', () => {
+    it('should return min of tb', () => {
+      assert.equal(moy([15, 16, 12, 17, 11]), 14.2);
+    });
+})
