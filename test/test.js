b94a45a431d359fcee31a436df4f208cfd939e0c
Tomsanchis <92373414+Tomsanchis@users.noreply.github.com>
2022-03-16 11:35:34 +0100

    describe for string remove

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 import assert from 'assert';
 import { min } from '../js/app.js';
 import { moy } from '../js/app.js';
+import { string } from '../js/app.js';
 
 describe('min', () => {
     it('should return min of tb', () => {
@@ -9,7 +10,13 @@ describe('min', () => {
 })
 
 describe('moy', () => {
-    it('should return min of tb', () => {
+    it('should return moyenne', () => {
       assert.equal(moy([15, 16, 12, 17, 11]), 14.2);
+    });
+})
+
+describe('string', () => {
+    it('should return remove string', () => {
+      assert.deepEqual(string(["plouk", 5, "ratata", 7]), [5, 7]);
     });
 })
